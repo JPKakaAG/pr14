97aea194a31b3657c0d564b2c33ad67ec11dc44d
JPKakaAG <Gjopka@yandex.ru>
2023-12-14 14:25:13 +0300

    Почти готово

---
+++ prac13/MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace prac13
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        public int[,] matrix;
+        private ContextMenu inputDataContextMenu;
+        private ContextMenu resultContextMenu;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeContextMenu();
+            gdProgram.Visibility = Visibility.Hidden;
+            gdLogin.Visibility = Visibility.Visible;
+            Loaded += MainWindow_Loaded;
+            // Добавление обработчика события к событию SelectedCellsChanged
+            DGarray.SelectedCellsChanged += dataGrid_SelectedCellsChanged;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Загрузка размера таблицы из файла конфигурации
+                (int rows, int columns) = ConfigurationSettings.LoadTableSize();
+
+                // Применение загруженного размера таблицы
+                int[,] matrix = new int[rows, columns];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке настроек: {ex.Message}");
+            }
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string password = tbPassword.Text;
+
+            if (password == "123")
+            {
+                gdProgram.Visibility = Visibility.Visible;
+                gdLogin.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                MessageBox.Show("Неверный пароль!");
+            }
+        }
+        private void InitializeContextMenu()
+        {
+            // Создаем контекстное меню для блока "Исходные данные"
+            inputDataContextMenu = new ContextMenu();
+
+            // Создаем пункт меню "Очистить"
+            MenuItem clearMenuItem = new MenuItem();
+            clearMenuItem.Header = "Очистить";
+            clearMenuItem.Click += ClearInputs_Click;
+            inputDataContextMenu.Items.Add(clearMenuItem);
+            // Назначаем контекстное меню блоку "Исходные данные"
+            GIntial.ContextMenu = inputDataContextMenu;
+
+            // Создаем контекстное меню для блока "Результат"
+            resultContextMenu = new ContextMenu();
+
+            MenuItem clearRezItem = new MenuItem();
+            clearRezItem.Header = "Очистить";
+            clearRezItem.Click += ClearRezult_Click;
+            // Назначаем контекстное меню блоку "Результат"
+            GRezult.ContextMenu = resultContextMenu;
+        }
+
+        private void ClearInputs_Click(object sender, RoutedEventArgs e)
+        {
+            // Очищаем значения текстовых полей блока "Исходные данные"
+            tbRows.Text = "";
+            tbColumn.Text = "";
+
+        }
+        private void ClearRezult_Click(object sender, RoutedEventArgs e)
+        {
+            DGarray.ItemsSource = null;
+            tblStatus.Text = "";
+            tbResult.Text = "";
+        }
+        private void btnSaveMatrix_Click(object sender, RoutedEventArgs e)
+        {
+            if (DGarray.ItemsSource != null)
+            {
+                MatrixManager manager = new MatrixManager();
+
+                // Сохранение матрицы в файл
+                manager.SaveMatrix(matrix);
+                MessageBox.Show("Матрица успешно сохранена.");
+            }
+            else
+            {
+                MessageBox.Show("Создай матрицу");
+                return;
+            }
+        }
+        private void btnOpenMatrix_CLick(object sender, RoutedEventArgs e)
+        {
+            MatrixManager manager = new MatrixManager();
+
+            int[,] matrix = manager.LoadMatrix();
+
+            if (matrix != null)
+            {
+                MessageBox.Show("Матрица успешно загружена:");
+                int rows = matrix.GetLength(0);
+                int columns = matrix.GetLength(1);
+
+                DGarray.ItemsSource = visulaarray.ToDataTable(matrix).DefaultView;
+            }
+            else
+            {
+                MessageBox.Show("Файл с матрицей не существует или матрица не была сохранена.");
+                return;
+            }
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if (DGarray.ItemsSource != null)
+            {
+                // Получение размеров матрицы
+                int rows = DGarray.Items.Count;
+                int columns = DGarray.Columns.Count;
+
+                // Создание двумерного массива для хранения матрицы
+                int[,] matrix = new int[rows, columns];
+
+                // Итерация по ячейкам DataGrid и заполнение массива матрицей
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        DataGridCellInfo cellInfo = new DataGridCellInfo(DGarray.Items[i], DGarray.Columns[j]);
+                        if (cellInfo.Column.GetCellContent(cellInfo.Item) is TextBlock cellContent)
+                        {
+                            if (int.TryParse(cellContent.Text, out int cellValue))
+                            {
+                                matrix[i, j] = cellValue;
+                            }
+                            else
+                            {
+                                // Обработка некорректных значений ячейки (например, если в ячейке не число)
+                            }
+                        }
+                    }
+                }
+                List<int> columnSums = new List<int>();
+                for (int col = 2; col <= columns; col += 2)
+                {
+                    int sum = 0;
+
+                    for (int row = 0; row < rows; row++)
+                    {
+                        sum += matrix[row, col - 1];
+                    }
+                    columnSums.Add(sum);
+                }
+                tbResult.Text = "Сумма чётных столбцов:\n" + string.Join("\n", columnSums);
+            }
+            else if (int.TryParse(tbRows.Text, out int rows) && int.TryParse(tbColumn.Text, out int columns))
+            {
+                if (rows > 0 && columns > 0)
+                {
+                    matrix = new int[rows, columns];
+
+                    visulaarray.Fill2Array(matrix);
+
+                    List<int> columnSums = new List<int>();
+
+                    for (int col = 2; col <= columns; col += 2)
+                    {
+                        int sum = 0;
+
+                        for (int row = 0; row < rows; row++)
+                        {
+                            sum += matrix[row, col - 1];
+                        }
+                        columnSums.Add(sum);
+                    }
+                    DGarray.ItemsSource = visulaarray.ToDataTable(matrix).DefaultView;
+                    tbResult.Text = "Сумма чётных столбцов:\n" + string.Join("\n", columnSums);
+                }
+                else
+                {
+                    MessageBox.Show("Введи положительные строки и столбцы");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введи строки и столбцы");
+            }
+
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил лучший разработчик: Девяткин Вадим Евгеньевич\r\nПрактическая №13 \r\n\r\nДаны координаты двух противоположных вершин прямоугольника: (x1, y1), (x2, y2).\r\nСтороны прямоугольника параллельны осям координат. Найти периметр и площадь данного прямоугольника.\r\n\r\nДан размер файла в байтах. Используя операцию деления нацело, найти количество полных килобайтов, которые занимает данный файл (1 килобайт = 1024 байта)");
+        }
+        private void dataGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            // Получение размеров таблицы
+            int rows = DGarray.Items.Count - 1;
+            int columns = DGarray.Columns.Count;
+
+            // Получение номера выделенной ячейки
+            int selectedRow = DGarray.SelectedIndex;
+            int selectedColumn = DGarray.CurrentCell.Column.DisplayIndex;
+
+            // Обновление строки статуса
+            tblStatus.Text = $"Размер таблицы: {rows}x{columns} | Выделенная ячейка: [{selectedRow + 1}, {selectedColumn + 1}]";
+        }
+    }
+
+}
---
+++ prac13/visulaarray.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Xml.Serialization;
+
+namespace prac13
+{
+    static class visulaarray
+    {
+        /// <summary>
+        /// Метод для двухмерного массива
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(this T[,] matrix)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var row = res.NewRow();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+
+                res.Rows.Add(row);
+            }
+
+            return res;
+        }
+        /// <summary>
+        /// Функция заполянет массив  числами от 0 до 10
+        /// </summary>
+        /// <param name="array"> имя массива для заполнения</param>
+        public static void Fill2Array(int[,] array)
+        {
+            // Получаем размеры массива
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+            Random rnd = new Random();
+
+            // Заполняем массив заданным значением
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = rnd.Next(0, 10);
+                }
+            }
+        }      
+    }
+    public class MatrixManager
+    {
+        private const string MatrixFilePath = "matrix.txt";
+
+        public void SaveMatrix(int[,] matrixData)
+        {
+            int rows = matrixData.GetLength(0);
+            int columns = matrixData.GetLength(1);
+
+            using (StreamWriter writer = new StreamWriter(MatrixFilePath))
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        writer.Write(matrixData[i, j] + " ");
+                    }
+                    writer.WriteLine();
+                }
+            }
+        }
+
+        public int[,] LoadMatrix()
+        {
+            if (File.Exists(MatrixFilePath))
+            {
+                using (StreamReader reader = new StreamReader(MatrixFilePath))
+                {
+                    string[] firstLineValues = reader.ReadLine().Split(' ');
+                    int rows = 1;
+                    int columns = firstLineValues.Length - 1;
+
+                    while (!reader.EndOfStream)
+                    {
+                        reader.ReadLine();
+                        rows++;
+                    }
+
+                    int[,] matrixData = new int[rows, columns];
+                    int currentRow = 0;
+
+                    using (StreamReader newReader = new StreamReader(MatrixFilePath))
+                    {
+                        while (!newReader.EndOfStream)
+                        {
+                            string[] values = newReader.ReadLine().Split(' ');
+
+                            for (int j = 0; j < columns; j++)
+                            {
+                                matrixData[currentRow, j] = int.Parse(values[j]);
+                            }
+
+                            currentRow++;
+                        }
+                    }
+
+                    return matrixData;
+                }
+            }
+
+            return null; // Если файл не существует или матрица не была сохранена, возвращаем null.
+        }
+    }
+    public static class ConfigurationSettings
+    {
+        private static string configFilePath = "config.ini";
+
+        public static void SaveTableSize(int[,] matrixData)
+        {
+            int rows = matrixData.GetLength(0);
+            int columns = matrixData.GetLength(1);
+            try
+            {
+                // Запись размера таблицы в файл конфигурации
+                string configData = $"{rows},{columns}";
+                File.WriteAllText(configFilePath, configData);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка при сохранении настроек: {ex.Message}");
+            }
+        }
+
+        public static (int rows, int columns) LoadTableSize()
+        {
+            try
+            {
+                if (File.Exists(configFilePath))
+                {
+                    // Чтение размера таблицы из файла конфигурации
+                    string configData = File.ReadAllText(configFilePath);
+                    string[] sizes = configData.Split(',');
+
+                    if (sizes.Length == 2 && int.TryParse(sizes[0], out int rows) && int.TryParse(sizes[1], out int columns))
+                    {
+                        return (rows, columns);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка при чтении настроек: {ex.Message}");
+            }
+
+            // Если файл конфигурации не существует или настройки не удалось прочитать, возвращаем значения по умолчанию
+            return (5, 5); // Пример значений по умолчанию
+        }
+    }
+}
